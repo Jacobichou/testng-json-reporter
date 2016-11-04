7bc8e614e1f3730dc3d3db6508796f1db107f574
Jacob <jacob.pagano@gmail.com>
2016-11-04 15:41:21 -0500

    initialized project

---
+++ src/main/java/xyz/neutrallemonade/testng_json_reporter/App.java
@@ -0,0 +1,13 @@
+package xyz.neutrallemonade.testng_json_reporter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
